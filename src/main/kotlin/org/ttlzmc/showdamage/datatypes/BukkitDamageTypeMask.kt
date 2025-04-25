c5d960bea17b805eafba985b508c6c80ac248091
maikeru-dev <dprofilo7@gmail.com>
2025-04-25 05:05:08 +0100

    rename BukkitDamageTypeMask.kt

--- src/main/kotlin/org/ttlzmc/showdamage/datatypes/BukkitDamageTypeMask.kt
+++ src/main/kotlin/org/ttlzmc/showdamage/datatypes/BukkitDamageTypeMask.kt
@@ -1,10 +1,12 @@
 import org.bukkit.damage.DamageType
 
-object BukkitDamageTypeMask {
-    private val mask = setOf( "wind_charge", "out_of_world", "outside_border", "thrown", "starve", "drown", "dry_out", "fireworks",)
+object BukkitDamageMask {
+    private val damageTypeMask = setOf( "wind_charge", "out_of_world", "outside_border", "thrown", "starve", "drown", "dry_out", "fireworks",)
 
     fun isValidDamageType(damageType: DamageType): Boolean {
         // Could improve performance here
-       return damageType.key.value() !in mask;
+       return damageType.key.value() !in damageTypeMask;
     }
+
+
 }
