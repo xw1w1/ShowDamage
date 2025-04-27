9d8df1b2aa6a89781c77bc55f74e77ea77aa0636
maikeru-dev <dprofilo7@gmail.com>
2025-04-27 15:21:21 +0100

    update chronounit to use seconds

--- src/main/kotlin/org/ttlzmc/showdamage/DamageDataCallback.kt
+++ src/main/kotlin/org/ttlzmc/showdamage/DamageDataCallback.kt
@@ -7,7 +7,7 @@ import java.util.concurrent.LinkedBlockingQueue
 
 class DamageDataCallback(val damageDisplay: DamageDisplay, val queue: LinkedBlockingQueue<DamageDataCallback>): Runnable{
     private val timeout = ShowDamageConfiguration.getDouble("visibility.popup-lifetime")
-    private val timeToExit = Instant.now().plus(timeout.toLong(), ChronoUnit.MILLIS)
+    private val timeToExit = Instant.now().plus(timeout.toLong(), ChronoUnit.SECONDS)
     override fun run() {
         if (Instant.now().isBefore(timeToExit)) {
             queue.put(this)
