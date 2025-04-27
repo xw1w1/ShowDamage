e326a3651c1c99d86fa6be19a5f9443b474dda18
maikeru-dev <dprofilo7@gmail.com>
2025-04-27 14:02:53 +0100

    fix assertion bug

--- src/main/kotlin/org/ttlzmc/showdamage/api/datatypes/DamageData.kt
+++ src/main/kotlin/org/ttlzmc/showdamage/api/datatypes/DamageData.kt
@@ -1,17 +1,17 @@
 package org.ttlzmc.showdamage.api.datatypes
 
 import org.bukkit.Location
-import org.bukkit.damage.DamageSource
-import org.ttlzmc.showdamage.api.BukkitDamageMask
+import org.bukkit.entity.Entity
+import org.bukkit.event.entity.EntityDamageEvent
 
 @Suppress("UnstableApiUsage")
 data class DamageData (
-    private val source: DamageSource,
-    private val damageType: DamageType,
+    private val source: Entity,
+    private val damageType: EntityDamageEvent.DamageCause,
     private val damageDealt: Double,
     private val damageRecord: DamageRecord
 ){
-    private val valid: Boolean = BukkitDamageMask.isValidDamageType(source.damageType)
+    private val valid: Boolean = true //FIXME:BukkitDamageMask.isValidDamageType(source.damageType)
     private val location = pushLocation(completeLocation(), y = 5.0)
 
     fun isValid(): Boolean {
@@ -24,7 +24,7 @@ data class DamageData (
 
     private fun completeLocation(): Location {
         val damagedEntities = damageRecord.getTargets()
-        check(damagedEntities.isEmpty()) { "Cannot calculate center for empty entity list. How did you even do that?" }
+        check(!damagedEntities.isEmpty()) { "Cannot calculate center for empty entity list. How did you even do that?" }
         if (damagedEntities.size == 1) return damagedEntities.first().location
         val world = damagedEntities.first().location.world
         val (totalX, totalY, totalZ) = damagedEntities.fold(Triple(0.0, 0.0, 0.0)) { (accX, accY, accZ), entity ->
